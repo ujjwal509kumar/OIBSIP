39ed492409af5b2e9b70a68fdfd04292d9609431
Ujjwal Kumar <68781093+ujjwal509kumar@users.noreply.github.com>
2023-04-19 08:26:23 +0530

    Add files via upload

---
+++ Level1/Task3/oasistest.js
@@ -0,0 +1,27 @@
+function convertTemp() {
+    var input = document.getElementById("degree").value;
+    var unit = document.getElementById("tempUnit").value;
+    var output = document.getElementById("result");
+    var tempresult, result, result1;
+    if (unit == "F" && isNaN(input)!=true) {
+      // convert Fahrenheit to Celsius
+      tempresult = (input - 32) * 5 / 9;
+      result = tempresult.toFixed(3);
+      result1 = result.toString();
+      result1 += " °C"
+      output.value = result1;
+    } else if (unit == "C" && isNaN(input)!=true) {
+      // convert Celsius to Fahrenheit
+      tempresult = input * 9 / 5 + 32;
+      result = tempresult.toFixed(3);
+      result1 = result.toString();
+      result1 += " °F"
+      output.value = result1;
+
+    } else {
+      // invalid unit
+      var errormsg = "Please enter a valid unit in numbers";
+      errormsg += " world!";
+      output.value = errormsg;
+    }
+  }
